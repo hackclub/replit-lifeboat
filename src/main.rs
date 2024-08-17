2facfd1e38c3e8be0144427d8c27a351b7f6dc9e
Malted <malted@malted.dev>
2024-08-17 14:58:43 -0400

    ✨ Download repls

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use graphql_client::{reqwest::post_graphql_blocking as post_graphql, GraphQLQuer
 use log::*;
 use reqwest::{blocking::Client, cookie::Jar, header, Url};
 use std::error::Error;
+use std::io::Write;
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -10,6 +11,14 @@ use std::error::Error;
     response_derives = "Debug"
 )]
 pub struct QuickUserQuery;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/graphql/schema 7.graphql",
+    query_path = "src/graphql/profilerepls-query.graphql",
+    response_derives = "Debug"
+)]
+pub struct ProfileRepls;
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -67,11 +76,40 @@ fn main() -> Result<(), Box<dyn Error>> {
             after: None,
         });
 
-    let mut repls_query = client.post(REPLIT_GQL_URL).json(&repls_query).send()?;
-    let repls_data: Response<repls_dashboard_repl_folder_list::ResponseData> =
-        repls_query.json()?;
+    // let repl_folder_data: Response<repls_dashboard_repl_folder_list::ResponseData> = client
+    //     .post(REPLIT_GQL_URL)
+    //     .json(&repls_query)
+    //     .send()?
+    //     .json()?;
 
-    info!("{:#?}", repls_data);
+    //#region Public repls
+    let mut profile_repls_data: Response<profile_repls::ResponseData> = client
+        .post(REPLIT_GQL_URL)
+        .json(&ProfileRepls::build_query(profile_repls::Variables {
+            after: None,
+        }))
+        .send()?
+        .json()?;
+
+    if let Some(profile_repls::ResponseData {
+        user:
+            Some(profile_repls::ProfileReplsUser {
+                profile_repls: profile_repls::ProfileReplsUserProfileRepls { items, page_info },
+            }),
+    }) = profile_repls_data.data
+    {
+        std::fs::create_dir_all("repls")?;
+        for repl in items.iter() {
+            let url = format!("https://replit.com{}.zip", repl.url);
+            info!("Downloading {} from {url}", repl.title);
+            let bytes = client.get(url).send()?.bytes()?;
+            debug!("{} is {} kB\n", repl.title, bytes.len() / 1_000);
+            let mut file = std::fs::File::create(format!("repls/{}.zip", &repl.title))?;
+            file.write_all(&bytes)?;
+        }
+    }
+
+    //#endregion
 
     Ok(())
 }
