57b636e1f3ad6b178fe3282acb837e8107ebcb37
PotentialStyx <62217716+PotentialStyx@users.noreply.github.com>
2024-08-17 15:17:18 -0700

    ✨Find all files

--- src/crosisdownload/mod.rs
+++ src/crosisdownload/mod.rs
@@ -4,10 +4,10 @@ use std::{sync::Arc, time::Duration};
 
 use anyhow::Result;
 use crosis::{
-    goval::{self, command::Body, Command, Ping, Pong},
-    Client,
+    goval::{self, command::Body, Command},
+    Channel, Client,
 };
-use log::debug;
+use log::{debug, error, info};
 use metadata::CookieJarConnectionMetadataFetcher;
 use reqwest::{cookie::Jar, header::HeaderMap};
 
@@ -60,7 +60,7 @@ pub async fn download(
     let gcsfiles = client.open("gcsfiles".into(), None, None).await?;
     dbg!(gcsfiles.id);
 
-    let res = gcsfiles
+    let mut fres = gcsfiles
         .request(Command {
             body: Some(Body::Readdir(goval::File {
                 path: ".".to_string(),
@@ -70,10 +70,60 @@ pub async fn download(
         })
         .await
         .unwrap();
+    dbg!(&fres);
 
-    dbg!(res);
+    let mut path = String::new();
+    let mut files_list = vec![];
+    let mut to_check_dirs = vec![];
+
+    loop {
+        if let Some(Body::Files(files)) = fres.body {
+            for file in files.files {
+                let fpath = if path.is_empty() {
+                    file.path.clone()
+                } else {
+                    path.clone() + "/" + &file.path
+                };
+
+                info!("{path} {} {fpath}", file.path);
+
+                match goval::file::Type::from_i32(file.r#type) {
+                    Some(goval::file::Type::Directory) => to_check_dirs.push(fpath),
+                    Some(goval::file::Type::Regular) => files_list.push(fpath),
+                    _ => {
+                        error!("bruh")
+                    }
+                }
+            }
+        }
+
+        dbg!(&to_check_dirs);
+
+        if let Some(npath) = to_check_dirs.pop() {
+            path = npath;
+
+            fres = gcsfiles
+                .request(Command {
+                    body: Some(Body::Readdir(goval::File {
+                        path: path.clone(),
+                        ..Default::default()
+                    })),
+                    ..Default::default()
+                })
+                .await
+                .unwrap();
+            dbg!(&fres);
+        } else {
+            break;
+        }
+    }
+
+    dbg!(files_list);
+    // dbg!(res);
 
     // client.close().await?;
 
     Ok(())
 }
+
+pub async fn handle_file(channel: Channel, filename: String, global_ts: u64) {}
