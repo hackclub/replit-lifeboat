c3b790a4d933e0b28ade199b5197fd3b7cb00a11
Malted <malted@malted.dev>
2024-08-23 01:15:46 +0100

    Experiment with replit auth

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
